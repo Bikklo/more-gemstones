0330b153ab946c62157096cad7a89941a9bfbb31
Bikklo <6206808+Bikklo@users.noreply.github.com>
2017-08-14 23:11:01 -0500

    Added lunge ability
    Added a lunge ability to the Amber sword, makes the player jump up and forwards in the direction theyre looking. Not sure if this ability will stay with the Amber sword, as I was only testing a concept.

--- src/main/java/com/bikklo/moregemstones/tools/ToolAmberSword.java
+++ src/main/java/com/bikklo/moregemstones/tools/ToolAmberSword.java
@@ -3,7 +3,13 @@ package com.bikklo.moregemstones.tools;
 import com.bikklo.moregemstones.MoreGemstonesMain;
 import com.bikklo.moregemstones.Reference;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 
 public class ToolAmberSword extends ItemSword {
 
@@ -13,6 +19,16 @@ public class ToolAmberSword extends ItemSword {
 		setUnlocalizedName(Reference.MoreGemstonesTools.TOOLAMBERSWORD.getUnlocalizedName());
 		setRegistryName(Reference.MoreGemstonesTools.TOOLAMBERSWORD.getRegistryName());
 		setCreativeTab(MoreGemstonesMain.tabMoreGemstones);
+	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+	{
+		Vec3d playerLook = playerIn.getLook(2.0F);
+		if(playerIn.isCollidedVertically == true) {
+			playerIn.addVelocity(playerLook.xCoord * 1.2D, playerIn.motionY + 0.75D, playerLook.zCoord * 1.2D);
+		}
+		return super.onItemRightClick(worldIn, playerIn, handIn);
 	}
 	
 }
